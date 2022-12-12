cfb82e65a7322250ab27ab3e8decccdf1eb6d173
timjini <hatim.jini@gmail.com>
2022-12-12 13:48:31 +0300

    redirect

--- server.js
+++
@@ -1,6 +0,0 @@
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) 
-    url = url.substring(1);
-   res.sendFile(url);
- });
