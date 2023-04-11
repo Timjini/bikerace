6e064a809d350badc9b792acf554a0b89dff7e20
Timjini <hatim.jini@gmail.com>
2023-04-11 22:37:20 +0300

    cors update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -21,8 +21,9 @@ function Map() {
     async function getData() {
       const res = axios ({
         method: 'get',
-        url: 'https://cors-anywhere.herokuapp.com/https://serene-castle-01441.herokuapp.com/api/v1/bikers',
+          url: 'https://serene-castle-01441.herokuapp.com/api/v1/bikers',
         headers: {
+          'Accept': 'application/json',
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
