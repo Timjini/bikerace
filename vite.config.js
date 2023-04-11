10075511bb2197b49e25be9310cbea94ae7a1614
Timjini <hatim.jini@gmail.com>
2023-04-11 22:44:55 +0300

    fixing proxy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    port: 3000,
+    open: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://serene-castle-01441.herokuapp.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [
     react()
   ],
