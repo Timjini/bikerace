468d8d42920e43ebd29c984afae858403862901e
Timjini <hatim.jini@gmail.com>
2023-04-18 09:56:25 +0300

    cors anywhere heroku

--- src/components/Bikers.jsx
+++ src/components/Bikers.jsx
@@ -1,30 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Bikers() {
-    const [data, setData] = useState(null);
-  
-    useEffect(() => {
-      axios.get('https://serene-castle-01441.herokuapp.com/api/v1/bikers')
-        .then((response) => {
-          setData(response.data);
-          console.log(response.data);
-        });
-    }, []);
-  
-  
-    return (
-      <div className='biker__container'>
-      {data && data.map((biker) => (
-        <div key={biker.id}>
-          <div className="biker__card">
-            <h1>{biker.first_name} {biker.last_name}</h1>
-            <p> City : {biker.city_of_origin}</p>
-            {biker.state_of_origin}
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('https://cors-anywhere.herokuapp.com/https://serene-castle-01441.herokuapp.com/api/v1/bikers', {
+        headers: {
+          'Origin': 'https://bikerace-colorado.vercel.app'
+        }
+      })
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="biker__container">
+      {data &&
+        data.map((biker) => (
+          <div key={biker.id}>
+            <div className="biker__card">
+              <h1>
+                {biker.first_name} {biker.last_name}
+              </h1>
+              <p>City: {biker.city_of_origin}</p>
+              <p>State: {biker.state_of_origin}</p>
+            </div>
           </div>
-        </div>
-      ))}
-  
-      </div>
-    );
-  }
+        ))}
+    </div>
+  );
+}
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,65 +1,66 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 import L from 'leaflet';
 
-
 function Map() {
-    const [data, setData] = useState(null);
-    const [loader, setLoader] = useState(true);
+  const [data, setData] = useState(null);
+  const [loader, setLoader] = useState(true);
 
-    //load the map after the data is loaded
+  useEffect(() => {
+    axios
+      .get('https://cors-anywhere.herokuapp.com/https://serene-castle-01441.herokuapp.com/api/v1/bikers', {
+        headers: {
+          'Origin': 'https://bikerace-colorado.vercel.app'
+          // 'Origin': 'http://localhost:5173'
+        }
+      })
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    useEffect (() => {
-      if (data) {
-        setLoader(false);
-      }
-    }, [data]);
+  useEffect(() => {
+    if (data) {
+      setLoader(false);
+    }
+  }, [data]);
 
-
-    useEffect(() => {
-      axios.get('https://serene-castle-01441.herokuapp.com/api/v1/bikers')
-        .then((response) => {
-          setData(response.data);
-          console.log(response.data);
-        });
-    }, []);
-  
-  //L icon for marker
   const icon = new L.Icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    shadowSize: [41, 41],
   });
 
-  
-  
-  
-    return (
-      <>
+  return (
+    <>
       {loader && <div className="loader"></div>}
-      
+
       <MapContainer center={[40.01, -105.26]} zoom={11} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {data && data.map((biker) => (
-            <Marker key={biker.id} position={[biker.latitude, biker.longitude]} icon={icon} >
-            <Popup>
-              {biker.first_name} {biker.last_name} <br />
-              {biker.state_of_origin} {biker.city_of_origin} <br />
-
-            </Popup>
-          </Marker>
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {data &&
+          data.map((biker) => (
+            <Marker key={biker.id} position={[biker.latitude, biker.longitude]} icon={icon}>
+              <Popup>
+                {biker.first_name} {biker.last_name} <br />
+                {biker.state_of_origin} {biker.city_of_origin} <br />
+              </Popup>
+            </Marker>
           ))}
       </MapContainer>
-            
-      </>
-    )
-  }
+    </>
+  );
+}
 
-export default Map
+export default Map;
